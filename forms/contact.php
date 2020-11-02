75163725be5fa81c8093be5a7c9a8790070e264e
danielvaab <danielvaab@unisabana.edu.co>
2020-11-02 17:48:43 -0500

    prueba 31

--- forms/contact.php
+++ forms/contact.php
@@ -26,16 +26,13 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
  
   $contact->smtp = array(
-    'host' => 'smtp@gmail.com',
-    'username' => 'danielva.empresa@gmail.com',
-    'password' => 'RealM888',
+    'SecureKey' => '75f8c6fe-2404-4a4c-a28e-6505bb290e69',
     'port' => '587'
   );
   
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['SecureKey'], 'From');
+
 
   echo $contact->send();
 ?>
